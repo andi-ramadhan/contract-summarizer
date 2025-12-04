8f1850e2730025988b00f6e93c7e760299deafab
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-12-04 17:31:37 +0700

    Remove SubmitButton component and summarize function

--- frontend/src/components/SubmitButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function SubmitButton({ text }) {
-  return (
-    <button className="mt-4 w-full bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700 hover:cursor-pointer">
-      {text}
-    </button>
-  );
-}
