5348a002da3803de57599abdfbdd25c361bb9f11
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-20 15:29:49 +0700

    trying to make the file upload formdata logic, backend not yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import Button from '@mui/material/Button'
 
 export default function App() {
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const res = await fetch('http://localhost:4000/upload', {
+      method: 'POST',
+      body: formData
+    });
+
+    console.log(await res.json());
+  }
+
+  // Note: Next try to make the backend sample for upload file
 
   return (
     <>
