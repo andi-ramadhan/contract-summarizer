aa412e50ad544c53006f15726f0be75b0ba94cb0
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-27 16:22:44 +0700

    feat: Add route for Results page to display analysis results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Upload from './pages/Upload';
 import './App.css'
+import Results from './pages/Results';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Upload />} />
+        <Route path='/results/:id' element={<Results />} />
       </Routes>
     </BrowserRouter>
   );
