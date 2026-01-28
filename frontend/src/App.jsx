0b236a3a34aacb6234b6b35985ec34b145fd0ea2
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-28 15:06:54 +0700

    feat: Update routing to set Home as the default page instead of Upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Upload from './pages/Upload';
 import './App.css'
 import Results from './pages/Results';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Upload />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/upload' element={<Upload />} />
         <Route path='/results/:id' element={<Results />} />
       </Routes>
     </BrowserRouter>
